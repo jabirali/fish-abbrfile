a50c6b9d0af0ca8e511dbe4a953d270d251bfe43
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-05-01 21:55:52 +0200

    Feat: Initial import from my dotfiles.

---
+++ conf.d/abbrfile.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+# Create abbreviations for every entry in `abbrfile`.
+if [ -f ~/.config/fish/abbrfile ]
+	for line in (sed '/^#/d' ~/.config/fish/abbrfile)
+		set -l dict (string split : $line)
+		if [ (count $dict) -ge 2 ]
+			set -l key (string trim $dict[1])
+			set -l val (string trim (string join : $dict[2..-1]))
+			abbr -ga $key $val
+		else
+			echo "abbrfile: Could not parse \"$line\"."
+		end
+	end
+end
