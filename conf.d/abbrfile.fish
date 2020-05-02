9497c66443477d2d4650c61d72a49edab5fd327a
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-05-02 07:23:27 +0200

    Fix: Whitespace bug.

--- conf.d/abbrfile.fish
+++ conf.d/abbrfile.fish
@@ -6,7 +6,7 @@ if [ -f ~/.config/fish/abbrfile ]
 		set -l dict (string split ' ' $line)
 		if [ (count $dict) -ge 2 ]
 			set -l key (string trim $dict[1])
-			set -l val (string trim (string join ' ' $dict[2..-1]))
+			set -l val $dict[2..-1]
 			abbr -ga $key $val
 		else if [ -n "$line" ]
 			echo "abbrfile: Could not parse \"$line\"."
