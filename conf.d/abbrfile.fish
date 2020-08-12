625e8e28971ccbcf41299ca9f35374336f5b4a85
Jabir Ali Ouassou <jabirali@switzerlandmail.ch>
2020-08-12 22:47:27 +0200

    Feat: Integrated `fish-prefer` functionality.
    The plugin `fish-prefer` was something I developed to be able to
    automatically e.g. expand `vi` to `vim` or `nvim` on systems where
    these are installed, change `grep` to `rg` if installed, etc. However,
    I much prefer the syntax of `fish-abbrfile` to `fish-prefer`, and
    would prefer to move my abbreviations out of `config.fish`.
    
    I've therefore integrated the core functionality here, and will retire
    my [`fish-prefer`](https://github.com/jabirali/fish-prefer) plugin. This
    also makes the code easier to maintain, by avoiding two similar plugins.

--- conf.d/abbrfile.fish
+++ conf.d/abbrfile.fish
@@ -5,9 +5,17 @@ if [ -f ~/.config/fish/abbrfile ]
 	for line in (sed '/^#/d' ~/.config/fish/abbrfile)
 		set -l dict (string split ' ' -- $line)
 		if [ (count $dict) -ge 2 ]
+      # Abbreviation that should trigger expansion.
 			set -l key (string trim -- $dict[1])
+      # Command to run when the triggering happens.
 			set -l val (string trim -- (string join ' ' -- $dict[2..-1]))
-			abbr -ga $key $val
+      # Only define abbreviation if the command is valid. This makes it
+      # possible to e.g. abbreviate `vi` to `vim` on systems where `vim`
+      # is available, but use the real `vi` if `vim` is not installed.
+      set -l cmd (string split --no-empty ' ' -- $val)
+      if [ (command -v $cmd[1]) ]
+		    abbr -ga $key $val
+      end
 		else if [ -n "$line" ]
 			echo "abbrfile: Could not parse \"$line\"."
 		end
